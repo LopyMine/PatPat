0863e8c018cdfdbac0cf3845e6dd8cd1167d644d
LopyMine <127966446+LopyMine@users.noreply.github.com>
2025-05-04 15:13:32 +0300

    Fixed publish workflow

--- buildSrc/src/main/java/net/lopymine/mossy/MossyPluginStonecutter.java
+++ buildSrc/src/main/java/net/lopymine/mossy/MossyPluginStonecutter.java
@@ -54,7 +54,7 @@ public class MossyPluginStonecutter implements Plugin<Project> {
 
 		tasks.register("publish+All", (task) -> {
 			controller.getVersions().forEach((version) -> {
-				task.dependsOn(":%s:publishModrinth".formatted(version.getProject()));
+				task.dependsOn(":%s:publishMods".formatted(version.getProject()));
 			});
 			task.setGroup("mossy-publish");
 		});
@@ -65,7 +65,7 @@ public class MossyPluginStonecutter implements Plugin<Project> {
 				if (!versionsSpecifications.contains(version.getProject())) {
 					return;
 				}
-				task.dependsOn(":%s:publishModrinth".formatted(version.getProject()));
+				task.dependsOn(":%s:publishMods".formatted(version.getProject()));
 			});
 			task.setGroup("mossy-publish");
 		});

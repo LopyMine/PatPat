62a1896a9e8a0e752c219eef754acf1a32908105
LopyMine <razvlekuha300@gmail.com>
2025-06-15 20:18:57 +0300

    Fixed compat with replays

--- src/main/java/net/lopymine/patpat/compat/replaymod/ReplayModCompat.java
+++ src/main/java/net/lopymine/patpat/compat/replaymod/ReplayModCompat.java
@@ -3,8 +3,6 @@ package net.lopymine.patpat.compat.replaymod;
 import net.fabricmc.fabric.api.networking.v1.*;
 
 import net.lopymine.patpat.client.PatPatClient;
-import net.lopymine.patpat.client.config.PatPatClientConfig;
-import net.lopymine.patpat.compat.LoadedMods;
 import net.lopymine.patpat.packet.s2c.*;
 
 //? <=1.20.1 {
@@ -15,10 +13,6 @@ public class ReplayModCompat {
 
 	public static void onPat(int pattedEntityId, int whoPattedId) {
 		PatPatClient.LOGGER.debug("Sending dummy packet for Replay Mod. [Patted: {} and Who: {}]", pattedEntityId, whoPattedId);
-		if (!LoadedMods.REPLAY_MOD_LOADED) {
-			PatPatClient.LOGGER.debug("Replay Mod not loaded");
-			return;
-		}
 		PatEntityForReplayModS2CPacketV2 packet = new PatEntityForReplayModS2CPacketV2(pattedEntityId, whoPattedId);
 		//? >=1.20.2 {
 		ReplayModManager.sendDummyPacket(ServerPlayNetworking.createS2CPacket(packet));
--- src/main/java/net/lopymine/patpat/compat/replaymod/ReplayModManager.java
+++ src/main/java/net/lopymine/patpat/compat/replaymod/ReplayModManager.java
@@ -2,6 +2,7 @@ package net.lopymine.patpat.compat.replaymod;
 
 import net.minecraft.network.protocol.Packet;
 import net.lopymine.patpat.client.PatPatClient;
+import net.lopymine.patpat.compat.LoadedMods;
 
 //? !(=1.20.5) {
 import com.replaymod.recording.ReplayModRecording;
@@ -13,6 +14,9 @@ public class ReplayModManager {
 
 	public static void sendDummyPacket(Packet<?> packet) {
 		//? !(=1.20.5) {
+		if (!LoadedMods.REPLAY_MOD_LOADED) {
+			return;
+		}
 		if (ReplayModRecording.instance.getConnectionEventHandler() != null) {
 			PatPatClient.LOGGER.debug("Sent dummy packet for Replay Mod");
 			PacketListener packetListener = ReplayModRecording.instance.getConnectionEventHandler().getPacketListener();
@@ -25,6 +29,9 @@ public class ReplayModManager {
 
 	public static boolean isInReplay() {
 		//? !(=1.20.5) {
+		if (!LoadedMods.REPLAY_MOD_LOADED) {
+			return false;
+		}
 		ReplayModReplay instance = ReplayModReplay.instance;
 		if (instance == null) {
 			return false;

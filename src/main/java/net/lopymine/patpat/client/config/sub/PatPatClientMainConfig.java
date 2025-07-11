c341461d1db48fcb7664a6c2028c3f4eb2f19ed4
nikita51bot <nikita.kjiaccuk@mail.ru>
2025-07-11 03:07:36 +0300

    Remove useless option in main config

--- src/main/java/net/lopymine/patpat/client/config/sub/PatPatClientMainConfig.java
+++ src/main/java/net/lopymine/patpat/client/config/sub/PatPatClientMainConfig.java
@@ -9,8 +9,6 @@ import net.lopymine.patpat.client.PatPatClient;
 import net.lopymine.patpat.client.keybinding.*;
 import net.lopymine.patpat.utils.*;
 
-import java.util.*;
-
 import static net.lopymine.patpat.utils.CodecUtils.option;
 
 @Getter
@@ -21,15 +19,12 @@ public class PatPatClientMainConfig {
 	public static final Codec<PatPatClientMainConfig> CODEC = RecordCodecBuilder.create(instance -> instance.group(
 					option("modEnabled", true, Codec.BOOL, PatPatClientMainConfig::isModEnabled),
 					option("debugLogEnabled", false, Codec.BOOL, PatPatClientMainConfig::isDebugLogEnabled),
-					option("patCombination", PatPatKeybinding.DEFAULT_COMBINATION, KeybindingCombination.CODEC, PatPatClientMainConfig::getPatCombination),
-					option("ignoredMobs", new HashSet<>(), Codec.STRING, PatPatClientMainConfig::getIgnoredMobs))
+					option("patCombination", PatPatKeybinding.DEFAULT_COMBINATION, KeybindingCombination.CODEC, PatPatClientMainConfig::getPatCombination))
 			.apply(instance, PatPatClientMainConfig::new));
 
 	private boolean modEnabled;
 	private boolean debugLogEnabled;
 	private KeybindingCombination patCombination;
-	@Setter(AccessLevel.PRIVATE)
-	private Set<String> ignoredMobs;
 
 	public static PatPatClientMainConfig getNewInstance() {
 		return CodecUtils.parseNewInstanceHacky(CODEC);

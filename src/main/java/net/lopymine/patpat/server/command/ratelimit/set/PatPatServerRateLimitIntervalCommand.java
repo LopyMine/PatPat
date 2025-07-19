40d8afac982b2bd1f0ba0da7971ad7c776d17895
nikita51bot <nikita.kjiaccuk@mail.ru>
2025-07-19 17:45:12 +0300

    Correct translation

--- src/main/java/net/lopymine/patpat/server/command/ratelimit/set/PatPatServerRateLimitIntervalCommand.java
+++ src/main/java/net/lopymine/patpat/server/command/ratelimit/set/PatPatServerRateLimitIntervalCommand.java
@@ -13,6 +13,7 @@ import net.lopymine.patpat.server.ratelimit.PatPatServerRateLimitManager;
 import net.lopymine.patpat.server.config.*;
 import net.lopymine.patpat.utils.*;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.Component;
 
@@ -22,6 +23,8 @@ import static net.minecraft.commands.Commands.literal;
 
 @ExtensionMethod(CommandExtension.class)
 public class PatPatServerRateLimitIntervalCommand {
+
+	private static final Object TIME_EXAMPLES_ARG = CommandText.getColoredArg(ChatFormatting.GRAY, "1sec, 5s, 5min");
 
 	private PatPatServerRateLimitIntervalCommand() {
 		throw new IllegalStateException("Command class");
@@ -51,7 +54,7 @@ public class PatPatServerRateLimitIntervalCommand {
 			Component text = CommandText.goldenArgs("ratelimit.set.interval", time).finish();
 			context.sendMsg(text);
 		} catch (IllegalArgumentException ignored) {
-			Component text = CommandText.goldenArgs("ratelimit.set.interval.not_time", value).finish();
+			Component text = CommandText.text("error.not_time", CommandText.getColoredArg(ChatFormatting.GOLD, value), TIME_EXAMPLES_ARG).finish();
 			context.sendMsg(text);
 			return 0;
 		}
